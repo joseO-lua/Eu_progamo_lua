d2077f2673005be143d2fc8922afc5587e3969b5
joseO-lua <joseotavio6573757717@gmail.com>
2026-03-19 22:54:33 -0300

    Add files via upload

--- amj.lua
+++ amj.lua
@@ -1,6 +1,11 @@
-function Sigma()
-  print("Olá, Mundo")
-    c = Sigma()
-  return c
-end 
-Sigma()
+--Criando projeto, data : 19/03/2026, 22:46
+
+function ParImp(num)
+  if num % 2 == 0 then
+    print('é par')
+else 
+  print('é impar')
+  end
+end
+
+
